d6dd935ea0822e9648e7850433ead1754a60b18c
Ahme281 <Ahme281@example.com>
2023-02-16 17:28:47 -0800

    mandatory tasks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A Programm that print with put function
+ * return :0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
