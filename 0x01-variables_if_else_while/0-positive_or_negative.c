735e9391b85112ed61e7a790c65919f6fbb0f5ce
Ahme281 <Ahme281@example.com>
2023-02-20 13:59:43 -0800

    test 2

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,21 +4,21 @@
 
 /**
  * main - Determines if a number is +ve or -ve or 0
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-        int n;
-
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
         if (n > 0)
         {
                 printf("%d is positive\n", n);
         }
         else if (n == 0)
         {
-                printf("%d is zero\n", n);
+	        printf("%d is zero\n", n);
         }
         else
         {
