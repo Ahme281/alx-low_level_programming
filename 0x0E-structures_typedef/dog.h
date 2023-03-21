4bd90cfa32c64dd20e82385f3e97460952cd7004
Ahme281 <Ahme281@example.com>
2023-03-20 18:38:50 -0700

    creat a new user defind variable by struct is called dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+/**
+ * struct dog - a new type struct dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *  Description: a new type that defines a dog with
+ *   its name, age and owner
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+#endif
